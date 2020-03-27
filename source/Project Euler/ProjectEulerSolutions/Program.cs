c600dcb881d519a75654f64f612913145f0e591f
Darren Pruitt <darren.pruitt@gmail.com>
2020-03-27 13:50:50 -0500

    Added my own Project Euler solutions

---
+++ source/Project Euler/ProjectEulerSolutions/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjectEulerSolutions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Problem 1 = {new Problem01().FindAnswer()}");
+        }
+    }
+}
