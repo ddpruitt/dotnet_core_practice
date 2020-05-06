60c1f4542152c9a42add180de8f762a2c2b2f9e4
Darren Pruitt <darren.pruitt@gmail.com>
2020-05-06 10:08:46 -0500

    Initial testing with custome attributes.

--- source/Patterns/Inversion of Control/POC.IoCItch/POC.IoCItch.Con01/Program.cs
+++ source/Patterns/Inversion of Control/POC.IoCItch/POC.IoCItch.Con01/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using StructureMap;
 
 namespace POC.IoCItch.Con01
 {
@@ -7,6 +9,42 @@ namespace POC.IoCItch.Con01
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            var container = new Container(_ =>
+            {
+                _.Scan(x =>
+                {
+                    x.TheCallingAssembly();
+
+                    x.AddAllTypesOf<IFantasySeries>();
+
+                    // or
+
+                    //x.AddAllTypesOf(typeof(IFantasySeries))
+                    //    .NameBy(type => type.Name.ToLower());
+                });
+            });
+
+            var classNames = container.Model.For<IFantasySeries>()
+                .Instances.Select(x => new { TypeName=x.ReturnedType.Name, CustomAttributes=x.ReturnedType.GetCustomAttributes(false)});
+
         }
+    }
+
+    public interface IFantasySeries { }
+
+    [Test(TestName = "Test1")]
+    public class WheelOfTime : IFantasySeries { }
+
+    [Test(TestName = "Test2")]
+    public class GameOfThrones : IFantasySeries { }
+
+    [Test(TestName = "Test3")]
+    public class BlackCompany : IFantasySeries { }
+
+    public class TestAttribute : Attribute
+    {
+        public string TestName { get; set; }
+
     }
 }
