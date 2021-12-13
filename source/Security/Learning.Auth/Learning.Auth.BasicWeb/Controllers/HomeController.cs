3ccbcfa000a385913e505b18f793e7201d9cfe43
Darren Pruitt <darren.pruitt@gmail.com>
2021-12-13 12:48:16 -0600

    changing projects to .net 6

--- source/Security/Learning.Auth/Learning.Auth.BasicWeb/Controllers/HomeController.cs
+++ source/Security/Learning.Auth/Learning.Auth.BasicWeb/Controllers/HomeController.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Security.Claims;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
